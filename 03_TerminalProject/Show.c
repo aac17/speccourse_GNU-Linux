1ec5791b514ba71f89d97411a9e8b1d2de530a21
Asi <ac17@bk.ru>
2020-10-04 23:29:36 +0300

    small changes

--- 03_TerminalProject/Show.c
+++ 03_TerminalProject/Show.c
@@ -6,6 +6,10 @@ int
 main (int argc, char * argv[])
 {
     setlocale(LC_ALL, "");
+    if (argc < 2) {
+        printf("Error: wrong arguments\n");
+        return 1;
+    }
     FILE *input = fopen(argv[1], "r");
     WINDOW *win;
     initscr ();
@@ -15,18 +19,22 @@ main (int argc, char * argv[])
     refresh();
     
     win = newwin(LINES - 6, COLS - 6, 3, 3);
-    keypad(win, TRUE);
+    //keypad(win, TRUE);
     scrollok(win, TRUE);
     int win_LINES = LINES - 6 - 2, win_COLS = COLS - 6 - 2;
+    if ((win_LINES <= 0) || (win_COLS <= 0)) {
+        printw("Error: small window");
+        endwin();
+        return 1;
+    }
     char str[win_COLS + 1];
     int flag = 0;
     int c = 32;
     do {
         if ((c == 32) && (flag == 0)) {
             werase(win);
-            box(win, 0, 0);
             for (int i = 0; i < win_LINES; i++) {
-                if (fgets(str, win_COLS, input) == NULL) {
+                if (fgets(str, win_COLS + 1, input) == NULL) {
                     flag = 1;
                     break;
                 }
